d2d8d2b8f12c16c56caca7f58cc2e4efa669fe6c
FromSouthToNorth <598050554@qq.com>
2020-09-01 16:26:50 +0800

    测试分页插件

--- ssm/src/test/java/com/hy/ssm/dao/ProvinceDAOTest.java
+++ ssm/src/test/java/com/hy/ssm/dao/ProvinceDAOTest.java
@@ -1,9 +1,12 @@
 package com.hy.ssm.dao;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.hy.ssm.BaseTest;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.List;
 import java.util.Map;
 
 public class ProvinceDAOTest extends BaseTest {
@@ -13,9 +16,10 @@ public class ProvinceDAOTest extends BaseTest {
 
     @Test
     public void findAll() {
-        for (Map<String, Object> stringObjectMap : provinceDAO.findAll()) {
-            System.out.println(stringObjectMap);
-        }
+        PageHelper.startPage(1, 2);
+        List<Map<String, Object>> all = provinceDAO.findAll();
+        PageInfo<Map<String, Object>> mapPageInfo = new PageInfo<>(all);
+        System.out.println(mapPageInfo);
     }
 
 }
