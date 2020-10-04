1cd827063f5625524687307b35ca4bc29fa6e9e5
FromSouthToNorth <598050554@qq.com>
2020-10-04 12:31:09 +0800

    ...

--- ssm/src/main/resources/staffing_system.sql
+++ ssm/src/main/resources/staffing_system.sql
@@ -1,17 +1,17 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : alyun
+ Source Server         : Hy
  Source Server Type    : MySQL
- Source Server Version : 50731
- Source Host           : 47.102.212.117:3306
+ Source Server Version : 80018
+ Source Host           : 127.0.0.1:3306
  Source Schema         : staffing_system
 
  Target Server Type    : MySQL
- Target Server Version : 50731
+ Target Server Version : 80018
  File Encoding         : 65001
 
- Date: 02/09/2020 10:32:28
+ Date: 04/10/2020 12:25:34
 */
 
 SET NAMES utf8mb4;
@@ -38,6 +38,47 @@ INSERT INTO `t_city` VALUES (4, '武汉', 2);
 INSERT INTO `t_city` VALUES (5, '十堰', 2);
 INSERT INTO `t_city` VALUES (6, '温州', 4);
 INSERT INTO `t_city` VALUES (7, '黄冈', 2);
+
+-- ----------------------------
+-- Table structure for t_logininfor
+-- ----------------------------
+DROP TABLE IF EXISTS `t_logininfor`;
+CREATE TABLE `t_logininfor`  (
+  `info_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `ipaddr` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `login_location` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `browser` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `os` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `status` char(1) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `msg` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `login_time` datetime(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`info_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for t_oper_log
+-- ----------------------------
+DROP TABLE IF EXISTS `t_oper_log`;
+CREATE TABLE `t_oper_log`  (
+  `oper_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `title` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `business_type` int(11) NULL DEFAULT NULL,
+  `method` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `request_method` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `operator_type` int(11) NULL DEFAULT NULL,
+  `oper_name` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `dept_name` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `oper_ip` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `oper_location` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `oper_param` varchar(2000) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `json_result` varchar(2000) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `status` int(11) NULL DEFAULT NULL,
+  `error_msg` varchar(2000) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `oper_time` datetime(0) NULL DEFAULT NULL,
+  `oper_url` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`oper_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for t_province
@@ -70,99 +111,22 @@ CREATE TABLE `t_staff`  (
   `province_id` bigint(20) NULL DEFAULT NULL,
   `city_id` bigint(20) NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 52 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 59 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of t_staff
 -- ----------------------------
-INSERT INTO `t_staff` VALUES (27, 'demo', 11, '男', NULL, 2, 4);
+INSERT INTO `t_staff` VALUES (27, '小红', 11, '男', NULL, 2, 4);
 INSERT INTO `t_staff` VALUES (28, '小明', 23, '女', NULL, 2, 5);
 INSERT INTO `t_staff` VALUES (29, '李四', 15, '男', NULL, 2, 4);
 INSERT INTO `t_staff` VALUES (34, '张三', 11, '男', NULL, 2, 5);
 INSERT INTO `t_staff` VALUES (47, 'Hy', 12, '男', NULL, 1, 1);
-
-SET FOREIGN_KEY_CHECKS = 1;
-/*
- Navicat Premium Data Transfer
-
- Source Server         : alyun
- Source Server Type    : MySQL
- Source Server Version : 50731
- Source Host           : 47.102.212.117:3306
- Source Schema         : staffing_system
-
- Target Server Type    : MySQL
- Target Server Version : 50731
- File Encoding         : 65001
-
- Date: 02/09/2020 10:32:28
-*/
-
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
-
--- ----------------------------
--- Table structure for t_city
--- ----------------------------
-DROP TABLE IF EXISTS `t_city`;
-CREATE TABLE `t_city`  (
-  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `name` varchar(80) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `province_id` bigint(20) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 8 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Records of t_city
--- ----------------------------
-INSERT INTO `t_city` VALUES (1, '成都', 1);
-INSERT INTO `t_city` VALUES (2, '广安', 1);
-INSERT INTO `t_city` VALUES (3, '北京市', 3);
-INSERT INTO `t_city` VALUES (4, '武汉', 2);
-INSERT INTO `t_city` VALUES (5, '十堰', 2);
-INSERT INTO `t_city` VALUES (6, '温州', 4);
-INSERT INTO `t_city` VALUES (7, '黄冈', 2);
-
--- ----------------------------
--- Table structure for t_province
--- ----------------------------
-DROP TABLE IF EXISTS `t_province`;
-CREATE TABLE `t_province`  (
-  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `name` varchar(80) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Records of t_province
--- ----------------------------
-INSERT INTO `t_province` VALUES (1, '四川');
-INSERT INTO `t_province` VALUES (2, '湖北');
-INSERT INTO `t_province` VALUES (3, '北京');
-INSERT INTO `t_province` VALUES (4, '浙江');
-
--- ----------------------------
--- Table structure for t_staff
--- ----------------------------
-DROP TABLE IF EXISTS `t_staff`;
-CREATE TABLE `t_staff`  (
-  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `name` varchar(80) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `age` int(11) NULL DEFAULT NULL,
-  `sex` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `avatar` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `province_id` bigint(20) NULL DEFAULT NULL,
-  `city_id` bigint(20) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 52 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Records of t_staff
--- ----------------------------
-INSERT INTO `t_staff` VALUES (27, 'demo', 11, '男', NULL, 2, 4);
-INSERT INTO `t_staff` VALUES (28, '小明', 23, '女', NULL, 2, 5);
-INSERT INTO `t_staff` VALUES (29, '李四', 15, '男', NULL, 2, 4);
-INSERT INTO `t_staff` VALUES (34, '张三', 11, '男', NULL, 2, 5);
-INSERT INTO `t_staff` VALUES (47, 'Hy', 12, '男', NULL, 1, 1);
+INSERT INTO `t_staff` VALUES (52, '999', 2, '男', NULL, 2, 4);
+INSERT INTO `t_staff` VALUES (53, '666', 66, '女', NULL, 2, 7);
+INSERT INTO `t_staff` VALUES (54, '小王八', 88, '女', NULL, 2, 4);
+INSERT INTO `t_staff` VALUES (55, '小贱人', 13, '男', NULL, 4, 6);
+INSERT INTO `t_staff` VALUES (56, '666', 22, '女', NULL, 1, 2);
+INSERT INTO `t_staff` VALUES (57, '55', 55, '女', NULL, 1, 1);
+INSERT INTO `t_staff` VALUES (58, '11', 4, '女', NULL, 2, 5);
 
 SET FOREIGN_KEY_CHECKS = 1;
